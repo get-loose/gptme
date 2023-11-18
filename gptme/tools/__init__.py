get-loose/gptme

commit f457f174fb2d4e3c9cd0293416f9ea2db03e9d27

fix: fixed handling codeblocks that contain triple backticks

gptme/tools/__init__.py
@@ -25,8 +25,7 @@ def execute_msg(msg: Message, ask: bool) -> Generator[Message, None, None]:
     assert msg.role == "assistant", "Only assistant messages can be executed"
 
     # get all markdown code blocks
-    codeblocks = [codeblock for codeblock in msg.content.split("```")[1::2]]
-    for codeblock in codeblocks:
+    for codeblock in get_codeblocks(msg.content):
         yield from execute_codeblock(codeblock, ask)
 
 
@@ -82,6 +81,12 @@ def is_supported_codeblock(codeblock: str) -> bool:
         return True
     else:
         return False
+
+
+def get_codeblocks(content: str) -> Generator[str, None, None]:
+    """Returns all codeblocks in a message."""
+    for codeblock in ("\n" + content).split("\n```")[1::2]:
+        yield codeblock
 
 
 def init_tools() -> None:

tests/test_util.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from gptme.tools import is_supported_codeblock
+from gptme.tools import get_codeblocks, is_supported_codeblock
 from gptme.util import epoch_to_age, generate_name, is_generated_name
 
 
@@ -38,3 +38,32 @@ print("hello world")
     # last block is plain/unsupported
     s = f"""{block_python}\n{block_plain}"""
     assert not is_supported_codeblock(s)
+
+
+def test_get_codeblocks():
+    s = """```python
+print("hello world")
+```
+"""
+
+    codeblocks = list(get_codeblocks(s))
+    assert len(codeblocks) == 1
+    assert (
+        codeblocks[0]
+        == """python
+print("hello world")"""
+    )
+
+    # test a codeblock which contains triple backticks
+    s = """```python
+print("hello ``` world")
+```
+"""
+
+    codeblocks = list(get_codeblocks(s))
+    assert len(codeblocks) == 1
+    assert (
+        codeblocks[0]
+        == """python
+print("hello ``` world")"""
+    )

commit 17900d397d8a24fc15a00fe5e9508012030cd798

fix: fixed regression in f457f17

gptme/tools/__init__.py
@@ -86,7 +86,7 @@ def is_supported_codeblock(codeblock: str) -> bool:
 def get_codeblocks(content: str) -> Generator[str, None, None]:
     """Returns all codeblocks in a message."""
     for codeblock in ("\n" + content).split("\n```")[1::2]:
-        yield codeblock
+        yield codeblock + "\n"
 
 
 def init_tools() -> None:

gptme/tools/save.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 from collections.abc import Generator
+from pathlib import Path
 
 from ..message import Message
 from ..util import ask_execute

tests/test_util.py
@@ -51,7 +51,8 @@ print("hello world")
     assert (
         codeblocks[0]
         == """python
-print("hello world")"""
+print("hello world")
+"""
     )
 
     # test a codeblock which contains triple backticks
@@ -65,5 +66,6 @@ print("hello ``` world")
     assert (
         codeblocks[0]
         == """python
-print("hello ``` world")"""
+print("hello ``` world")
+"""
     )